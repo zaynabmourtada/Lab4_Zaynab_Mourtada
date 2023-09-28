21dd5808d4481e8a1e21196df5b6263fa9f517b7
Meriam harissa <mharissa@umich.edu>
2023-09-28 18:32:29 -0400

    Modified Hello.java with a new greeting.

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ public class Hello {
         
         for(int i=1;i<=12;i++) {
            
-            System.out.println("Hello World..."+i);
+            System.out.println("Greetings from Meriam..."+i);
 }
 }
 }
