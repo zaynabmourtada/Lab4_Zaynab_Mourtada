37e673477c64a42fd610f455102640d602a35983
malakm <malakm@umich.edu>
2023-09-28 16:35:22 -0400

    i is now 14

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         
-        for(int i=1;i<=12;i++) {
+        for(int i=1;i<=14;i++) {
            
             System.out.println("Hello World..."+i);
-}
-}
+        }
+    }
 }
 
